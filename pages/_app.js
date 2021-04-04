889fc5bb03bf806cb153cef4201baa8b03529b1c
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-04-04 14:36:14 +0800

    fix esbuild error in netlify

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+if (__dirname === '' && !process.env.ESBUILD_BINARY_PATH) {
+  if (process.platform === 'win32') {
+    process.env.ESBUILD_BINARY_PATH = path.join(
+      process.cwd(),
+      'node_modules',
+      'esbuild',
+      'esbuild.exe'
+    );
+  } else {
+    process.env.ESBUILD_BINARY_PATH = path.join(
+      process.cwd(),
+      'node_modules',
+      'esbuild',
+      'bin',
+      'esbuild'
+    );
+  }
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
