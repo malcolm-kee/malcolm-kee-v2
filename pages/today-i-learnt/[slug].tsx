d090abaed5b483b404d5e581fac4ce41c8c02ce0
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-04-04 13:23:27 +0800

    migrate til

---
+++ pages/today-i-learnt/[slug].tsx
@@ -0,0 +1,88 @@
+import fs from 'fs';
+import glob from 'glob';
+import { bundleMDX } from 'mdx-bundler';
+import { Seo } from 'components/seo';
+import { getMDXComponent } from 'mdx-bundler/client';
+import type { GetStaticPaths, GetStaticProps } from 'next';
+import path from 'path';
+import * as React from 'react';
+
+interface TodayILearntProps {
+  mdx?: MdxResult;
+}
+
+const components = {
+  code: function Code({ children }: { children?: React.ReactNode }) {
+    return <code className="language-text">{children}</code>;
+  },
+};
+
+function TodayILearnt({ mdx }: TodayILearntProps) {
+  const Component = React.useMemo(
+    () => (mdx ? getMDXComponent(mdx.code) : null),
+    [mdx]
+  );
+
+  return (
+    <div className="px-4 sm:px-6 py-6">
+      {mdx && <Seo title={`TIL ${mdx.frontmatter.title} - Malcolm Kee`} />}
+      <article className="pb-12">
+        <div className="prose max-w-prose mx-auto">
+          {mdx && <h1>{mdx.frontmatter.title}</h1>}
+          {Component && <Component components={components} />}
+        </div>
+      </article>
+    </div>
+  );
+}
+
+type MdxResult = {
+  code: string;
+  frontmatter: Record<string, any>;
+};
+
+type Params = {
+  slug: string;
+};
+
+const tilPath = path.resolve(process.cwd(), 'til').split(path.sep).join('/');
+
+export const getStaticProps: GetStaticProps<
+  TodayILearntProps,
+  Params
+> = async function getStaticProps({ params }) {
+  if (params) {
+    const fileContent = await fs.promises.readFile(
+      path.resolve(tilPath, `${params.slug}.mdx`),
+      'utf-8'
+    );
+    const mdxResult = await bundleMDX(fileContent);
+
+    return {
+      props: {
+        mdx: mdxResult,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
+
+export const getStaticPaths: GetStaticPaths<Params> = async function getStaticPaths() {
+  const results = glob.sync(`${tilPath}/**/*.{md,mdx}`);
+
+  return {
+    paths: results.map((result) => {
+      return {
+        params: {
+          slug: path.parse(result).name,
+        },
+      };
+    }),
+    fallback: false,
+  };
+};
+
+export default TodayILearnt;
