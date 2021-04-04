4cab42fb19c5cccf7d0573be03444223e3d0c194
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-04-04 19:50:04 +0800

    debug esbuild (#2)
    * debug esbuild
    
    * debug netlify
    
    * try fix netlify build
    
    * remove console.log

--- lib/prepare-mdx.ts
+++ lib/prepare-mdx.ts
@@ -2,7 +2,7 @@ import { bundleMDX } from 'mdx-bundler';
 import path from 'path';
 
 export const prepareMdx = (source: string) => {
-  if (__dirname === '' && !process.env.ESBUILD_BINARY_PATH) {
+  if (process.env.NETLIFY && !process.env.ESBUILD_BINARY_PATH) {
     if (process.platform === 'win32') {
       process.env.ESBUILD_BINARY_PATH = path.join(
         process.cwd(),
